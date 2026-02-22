96b6f67cd693a92d0e0bac5213574e9766f80a3a
TamașRV <tamasr09@gmail.com>
2026-02-22 15:10:37 +0200

    sin plot in xOy com3

--- main.py
+++ main.py
@@ -1,11 +1,28 @@
 import math
+import matplotlib.pyplot as plt
+import numpy as np
 
-INTERVAL = [0, 2*math.pi]
+# Your settings
+INTERVAL = [-2*math.pi, 2*math.pi] # Expanded to show the origin clearly
+N = 500 
 
-N = 500 #sample points
+def f(x):
+    return math.sin(x)
 
-a = -1000
-b = 1000
+x = np.linspace(INTERVAL[0], INTERVAL[1], N)
+y = [f(i) for i in x]
 
-step_size = (b-a)/N
+fig, ax = plt.subplots(figsize=(8, 5))
 
+# The "xOy" Magic
+ax.spines['left'].set_position('zero')
+ax.spines['bottom'].set_position('zero')
+ax.spines['right'].set_color('none')
+ax.spines['top'].set_color('none')
+
+# Plotting
+ax.plot(x, y, label='function', color='blue', linewidth=2)
+ax.grid(True, linestyle='--', alpha=0.6)
+ax.legend()
+
+plt.show()
